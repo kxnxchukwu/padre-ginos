cf89de6843379bc13f22840e0924056d9922ccc4
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-01 23:25:13 +0000

    Add StrictMode and PizzaOfTheDay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { createElement } from "react";
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
+import { StrictMode } from "react";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
-    <div>
-      <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="Margherita"
-        toppings={["tomato", "mozzarella"]}
-        image="/public/pizzas/big_meat.webp"
-      />
-      <Pizza
-        name="Pepperoni"
-        toppings="pepperoni"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        toppings={["ham", "pineapple"]}
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-    </div>
+    <StrictMode>
+      <div>
+        <h1 className="logo">Padre Gino's</h1>
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </StrictMode>
   );
 };
 
