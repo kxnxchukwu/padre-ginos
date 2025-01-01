26b043a5209f09a5bb5f4cce90ca844240ed6eaa
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-01 09:58:04 +0000

    Updated eslint defaults

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,35 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
   js.configs.recommended,
   {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
+    },
+    parserOptions: {
+      ecmaVersion: 2022,
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    rules: {
+      "react/no-unescape-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
