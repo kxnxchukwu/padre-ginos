123cfbac48fedb78eba2092ecb3d232d320c949f
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-01 23:31:31 +0000

    Add usePizzaOfTheDay Custom Hook

---
+++ src/usePizzaOfTheDay.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { PizzaType } from "../types";
+
+export default function usePizzaOfTheDay(): PizzaType | null {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState<PizzaType | null>(null);
+
+  async function fetchPizzaOfTheDay(): Promise<void> {
+    const response = await fetch("/api/pizza-of-the-day");
+    const data = await response.json();
+    setPizzaOfTheDay(data);
+  }
+
+  useEffect(() => {
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+}
