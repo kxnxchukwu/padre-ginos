c8b5b007794c4076764be3060bd9186f8e1333e1
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-02 10:31:24 +0000

    Added Modal, ErrorBoundary to PastOrderRoute

--- src/routes/past.lazy.tsx
+++ src/routes/past.lazy.tsx
@@ -2,17 +2,38 @@ import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { ReactElement, useState } from "react";
 import getPastOrders from "../api/getPastOrders";
+import getPastOrder from "../api/getPastOrder";
+import { getIntl } from "../utils/intlUtil";
+import Modal from "../Modal";
+import ErrorBoundary from "../ErrorBoundary";
 
 export const Route = createLazyFileRoute("/past")({
-  component: PastOrderRoute,
+  component: ErrorBoundaryWrappedPastOrderRoute,
 });
+
+function ErrorBoundaryWrappedPastOrderRoute(): ReactElement {
+  return (
+    <ErrorBoundary>
+      <PastOrderRoute />
+    </ErrorBoundary>
+  );
+}
 
 function PastOrderRoute(): ReactElement {
   const [page, setPage] = useState(1);
+  const intl = getIntl();
+  const [focusedOrder, setFocusedOrder] = useState<string | null>(null);
   const { isLoading, data } = useQuery({
     queryKey: ["past-orders", page],
     queryFn: () => getPastOrders(page),
     staleTime: 600000,
+  });
+
+  const { isLoading: isLoadingFocusedData, data: focusedOrderData } = useQuery({
+    queryKey: ["past-order", focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder as string),
+    staleTime: 24 * 60 * 60 * 1000,
+    enabled: !!focusedOrder,
   });
 
   if (isLoading) {
@@ -36,7 +57,11 @@ function PastOrderRoute(): ReactElement {
         <tbody>
           {data?.map((order) => (
             <tr key={order.order_id}>
-              <td>{order.order_id}</td>
+              <td>
+                <button onClick={() => setFocusedOrder(order.order_id)}>
+                  {order.order_id}
+                </button>
+              </td>
               <td>{order.date}</td>
               <td>{order.time}</td>
             </tr>
@@ -55,6 +80,44 @@ function PastOrderRoute(): ReactElement {
           Next
         </button>
       </div>
+      {focusedOrder ? (
+        <Modal>
+          <>
+            <h2>Order #{focusedOrder}</h2>
+            {!isLoadingFocusedData ? (
+              <table>
+                <thead>
+                  <tr>
+                    <td>Image</td>
+                    <td>Name</td>
+                    <td>Size</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                    <td>Total</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {focusedOrderData?.orderItems.map((pizza) => (
+                    <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                      <td>
+                        <img src={pizza.image} alt={pizza.name} />
+                      </td>
+                      <td>{pizza.name}</td>
+                      <td>{pizza.size}</td>
+                      <td>{pizza.quantity}</td>
+                      <td>{intl.format(pizza.price)}</td>
+                      <td>{intl.format(pizza.total)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p>Loading …</p>
+            )}
+            <button onClick={() => setFocusedOrder(null)}>Close</button>
+          </>
+        </Modal>
+      ) : null}
     </div>
   );
 }
