49b0d3385124d898bbdef05d32b0a471bd9a8df3
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-02 08:49:08 +0000

    Modified Header, uses cart context instead of hard coded value. Add link from tanstack/router

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,17 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
+import { Link } from "@tanstack/react-router";
+import { CartContext } from "./contexts";
 
 export default function Header(): ReactElement {
+  const [cart] = useContext(CartContext);
   return (
     <nav>
-      <h1 className="logo">Padre Gino's Pizza</h1>
+      <Link to={"/"}>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+      </Link>
       <div className="nav-cart">
         🛒
-        <span className="nav-cart-number">12</span>
+        <span className="nav-cart-number">{cart.length}</span>
       </div>
     </nav>
   );
