97710fe8a653f24414dac3eef8e1e0dd96bbcbef
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-03 15:20:44 +0000

    Add ImportMeta interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,9 @@
 declare module "@tanstack/router-plugin/vite";
 declare module "@tanstack/react-router";
 declare module "@tanstack/router-devtools";
+
+interface ImportMeta {
+  env: {
+    VITE_API_URL: string;
+  };
+}
