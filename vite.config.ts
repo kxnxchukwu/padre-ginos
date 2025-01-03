c1ac0143c7742168a5ece3b65a571e3098d01e0e
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-03 16:10:16 +0000

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,36 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default ({ mode }: { mode: string }) => {
+  const loadedEnv = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-  plugins: [
-    TanStackRouterVite(),
-    react({
-      babel: {
-        plugins: [
-          [
-            "babel-plugin-react-compiler",
-            {
-              target: "19",
-            },
+    plugins: [
+      TanStackRouterVite(),
+      react({
+        babel: {
+          plugins: [
+            [
+              "babel-plugin-react-compiler",
+              {
+                target: "19",
+              },
+            ],
           ],
-        ],
-      },
-    }),
-  ],
-});
+        },
+      }),
+    ],
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(loadedEnv.VITE_API_URL),
+    },
+  });
+};
