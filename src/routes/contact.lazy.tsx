cbc33829e3738ad7f10c8847dc1c09e2fa80cebe
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-01-03 14:15:13 +0000

    Removed Link Component from tanstack/react-router

--- src/routes/contact.lazy.tsx
+++ src/routes/contact.lazy.tsx
@@ -1,7 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { FormEvent, ReactElement } from "react";
-import { Link } from "@tanstack/react-router";
 import postContact from "../api/postContact";
 
 export const Route = createLazyFileRoute("/contact")({
@@ -25,9 +24,6 @@ function ContactRoute(): ReactElement {
     return (
       <>
         <h2>Something went wrong. Please try again later.</h2>
-        <p>
-          Go back <Link to="/">/home</Link>
-        </p>
       </>
     );
   }
@@ -36,15 +32,15 @@ function ContactRoute(): ReactElement {
     <div className="contact">
       <h2>Contact</h2>
       {mutation.isSuccess ? (
-        <h3>
-          Submitted. You can navigate back to <Link to="/">/home</Link>
-        </h3>
+        <h3>Submitted!!!</h3>
       ) : (
         <form onSubmit={mutation.mutate}>
           <input type="text" name="name" placeholder="Name" />
           <input type="email" name="email" placeholder="Email" />
           <textarea name="message" placeholder="Message" />
-          <button type="submit">Submit</button>
+          <button role="button" type="submit">
+            Submit
+          </button>
         </form>
       )}
     </div>
